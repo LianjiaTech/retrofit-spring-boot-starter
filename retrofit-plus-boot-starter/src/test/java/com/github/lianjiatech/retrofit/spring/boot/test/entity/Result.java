3cec98faab91704707bfdcafcb2ba399f8bae2f8
陈添明 <CHENTIANMING002@ke.com>
2020-04-04 10:36:02 +0800

    包名rename

---
+++ retrofit-plus-boot-starter/src/test/java/com/github/lianjiatech/retrofit/spring/boot/test/entity/Result.java
@@ -0,0 +1,21 @@
+package com.github.lianjiatech.retrofit.spring.boot.test.entity;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * @author 陈添明
+ */
+@Data
+@Accessors(chain = true)
+public class Result<T> implements Serializable {
+
+    private static final long serialVersionUID = -504027247149928390L;
+
+    private int code;
+    private String msg;
+    private String exceptionMsg;
+    private T data;
+}
---
+++ retrofit-plus-boot-starter/src/test/java/com/github/lianjiatech/retrofit/spring/boot/test/interceptor/PrintInteceptor.java
@@ -0,0 +1,21 @@
+package com.github.lianjiatech.retrofit.spring.boot.test.interceptor;
+
+import com.github.lianjiatech.retrofit.plus.interceptor.BaseGlobalInterceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * @author 陈添明
+ */
+@Component
+public class PrintInteceptor extends BaseGlobalInterceptor {
+    @Override
+    public Response doIntercept(Chain chain) throws IOException {
+        Request request = chain.request();
+        System.out.println("=============test===========");
+        return chain.proceed(request);
+    }
+}
