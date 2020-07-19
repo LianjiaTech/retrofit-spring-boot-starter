b6d2a1cef0901df407faa2fd146667e3b498eacc
陈添明 <chentianming11@qq.com>
2020-07-19 13:44:44 +0800

    update readme

---
+++ src/test/java/com/github/lianjiatech/retrofit/spring/boot/test/interceptor/SourceInterceptor.java
@@ -0,0 +1,20 @@
+package com.github.lianjiatech.retrofit.spring.boot.test.interceptor;
+
+import com.github.lianjiatech.retrofit.spring.boot.interceptor.BaseGlobalInterceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class SourceInterceptor extends BaseGlobalInterceptor {
+    @Override
+    public Response doIntercept(Chain chain) throws IOException {
+        Request request = chain.request();
+        Request newReq = request.newBuilder()
+                .addHeader("source", "test")
+                .build();
+        return chain.proceed(newReq);
+    }
+}
