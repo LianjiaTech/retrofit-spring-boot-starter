a1714aba058d068b6cb25a0fecd07950b6204e11
tm.chen <tm.chen@trip.com>
2022-01-19 17:41:05 +0800

    在开启全局重试的情况下，支持指定某些接口手动关闭重试

--- src/test/java/com/github/lianjiatech/retrofit/spring/boot/test/http/HttpApi.java
+++ src/test/java/com/github/lianjiatech/retrofit/spring/boot/test/http/HttpApi.java
@@ -2,6 +2,7 @@ package com.github.lianjiatech.retrofit.spring.boot.test.http;
 
 import com.github.lianjiatech.retrofit.spring.boot.annotation.Intercept;
 import com.github.lianjiatech.retrofit.spring.boot.annotation.RetrofitClient;
+import com.github.lianjiatech.retrofit.spring.boot.retry.Retry;
 import com.github.lianjiatech.retrofit.spring.boot.test.entity.Person;
 import com.github.lianjiatech.retrofit.spring.boot.test.entity.Result;
 import com.github.lianjiatech.retrofit.spring.boot.test.ex.TestErrorDecoder;
@@ -104,6 +105,7 @@ public interface HttpApi {
      * @return .
      */
     @POST("error")
+    @Retry(enable = false)
     Person error(@Body Person person);
 
     /**
