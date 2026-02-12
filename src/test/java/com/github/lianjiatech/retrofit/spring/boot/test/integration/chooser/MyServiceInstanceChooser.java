fa47c4be158742ac96035db97cde70c090ce78c7
tm.chen <tm.chen@trip.com>
2026-02-12 16:04:18 +0800

    ServiceInstanceChooser newly supports request parameter

--- src/test/java/com/github/lianjiatech/retrofit/spring/boot/test/integration/chooser/MyServiceInstanceChooser.java
+++ src/test/java/com/github/lianjiatech/retrofit/spring/boot/test/integration/chooser/MyServiceInstanceChooser.java
@@ -1,6 +1,7 @@
 package com.github.lianjiatech.retrofit.spring.boot.test.integration.chooser;
 
 import com.github.lianjiatech.retrofit.spring.boot.core.ServiceInstanceChooser;
+import okhttp3.Request;
 import org.springframework.stereotype.Component;
 
 import java.net.URI;
@@ -13,7 +14,7 @@ import java.net.URI;
 public class MyServiceInstanceChooser implements ServiceInstanceChooser {
 
     @Override
-    public URI choose(String serviceId) {
+    public URI choose(String serviceId, Request request) {
         if (serviceId.equals("user")) {
             return URI.create("http://localhost:8080");
         }
